f2f92f94fa66f8eae57c7ef6b6e27d49505306c0
Maria  Clara Martins Marinelli <11208594680@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-27 09:32:55 -0300

    Front-controller

--- controller/categoria.php
+++ controller/categoria.php
@@ -1,11 +1,28 @@
 <?php
-    require "../conexao.php";
-    require "../model/categoria_model.php";
+    require "model/CategoriaModel.php";
 
-    $model = new CategoriaModel($con);
+    class Categoria{
+        
+        function __construct(){
+            $this->model = new CategoriaModel();
+        }
+
+        function index(){
+            var_dump($this->model->buscarTodos());
+        }
+
+        function inserir(){
+            echo "testando função inserir";
+        }
+    }
+
+    //$controller = new Categoria();
+    //$controller->index();
+
+    //$model = new CategoriaModel();
     //$model->inserir("Produto de Limpeza");
     //$model->excluir(2);
     //$model->atualizar("Smartphone", 3);
-    var_dump($model->buscarPorId(3));
+    //var_dump($model->buscarPorId(3));
 
 ?>
